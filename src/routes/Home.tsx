0c006b7d65fdf7221d87ab0a70652329eeaabf55
Bruno Rennan <bruno_rennan@hotmail.com>
2023-06-30 22:12:55 -0300

    modificação da home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -7,10 +7,24 @@ const Home = () => {
     <div className={styles.home}>
       <div className={styles.intro}>
         <h1>Bem-vindo ao meu portfólio</h1>
-        <p>Bruno Rennan / Desenvolvedor Web</p>
+        <p>Desenvolvedor Full Stack</p>
+        <div className={styles.text}>
+          <p>
+            Olá, me chamo Bruno Rennan, desenvolvedor Full Stack com experiência em Node.js,
+            JavaScript, TypeScript, React, HTML e CSS. Especializado em criação
+            de aplicativos web completos. Pronto para desenvolver projetos de
+            alta qualidade.
+          </p>
+          <Link to="https://github.com/RennanHolanda">
+            <img src="public\img\github-original.svg" alt="github" />
+          </Link>
+          <Link to="https://www.linkedin.com/in/rennanholanda/">
+            <img src="public\img\linkedin-plain.svg" alt="linkedin" />
+          </Link>
+        </div>
       </div>
       <div className={styles.profile}>
-        <Link style={{textDecoration: 'none'}} to={"/contact"}>
+        <Link style={{ textDecoration: "none" }} to={"/contact"}>
           <p>
             <BsArrowReturnRight />
             Entre em contato
